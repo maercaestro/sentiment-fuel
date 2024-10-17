fcf53d0932e2aa867f146ce224e6f23429e8a937
maercaestro <maercaestro@gmail.com>
2024-10-17 08:58:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,6 @@ MIT License
 
 Copyright (c) 2024 Abu Huzaifah Bin Haji Bidin
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
 
 """
 
@@ -100,6 +82,7 @@ scale_factor = 0.15  # 15% of the original size
 new_width = int(image2.width * scale_factor)
 new_height = int(image2.height * scale_factor)
 resized_image = image2.resize((new_width, new_height))
+
 
 st.set_page_config(layout="wide", page_title="Fuel Station Insights Dashboard")
 # Add image to the top right corner
